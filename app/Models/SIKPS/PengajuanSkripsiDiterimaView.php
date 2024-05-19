bd9adaeeb58c7bb8d2b983547dad1e3a758be964
suhaefifzn <suhaefi21@gmail.com>
2024-05-19 10:53:27 +0700

    update model for api si-wisuda

---
+++ app/Models/SIKPS/PengajuanSkripsiDiterimaView.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\SIKPS;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PengajuanSkripsiDiterimaView extends Model
+{
+    /**
+     * Model ini mengarah ke view pengajuan skripsi diterima di db simak baru skema sikps
+     * Digunakan untuk mengelola proses read atau select
+     */
+    use HasFactory;
+
+    protected $table = 'sikps.vpengajuan_skripsi_diterima';
+    protected $connection;
+
+    public $timestamps = false;
+
+    public function __construct()
+    {
+        $this->connection = config('myconfig.database.first_connection');
+    }
+
+    public function scopeGetPengajuanSkripsi(Builder $query, $nim)
+    {
+        return $query->where('nim', $nim)->orderBy('created_at', 'DESC')->first();
+    }
+}
